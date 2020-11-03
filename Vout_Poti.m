2fd82d07d574da1882f85d38d11f563dcce706c0
mgvargas <minervavargasg@gmail.com>
2020-11-03 09:27:45 +0100

    Calculating Dig.Poti resolution

--- Vout_Poti.m
+++ Vout_Poti.m
@@ -1,8 +1,21 @@
-Rx=5000;
+% Calculations for 20k Digital potentiometer AD5270
+% 1024 position resolution
+
+% Set res. value --> obtained voltage
+
+Rx=6000;  % Value potentiometer
 Vin=1;
 Rbelow =(20000+Rx)*16000/(20000+Rx+16000);
 Rtot = Rbelow+10000;
+
+% Voltage to the amplifier: Vin+ to INA (Signal_ref_A)
 Vout = Rbelow/Rtot*Vin
 
+% Resolution (min. resistance change) 20 Ohms
+step = 20000/1024; % range / steps
+% 1 step increases 0.1mV
+
 hex2dec('f1ff');
-binaryVectorToHex([0 0 0 0 1 1 0 0])
+binaryVectorToHex([0 0 0 0 1 1 0 0]);
+hexToBinaryVector('23');
+dec2hex(12);
